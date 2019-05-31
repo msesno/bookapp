e431d3e647a984318426e9b6d9cd2f258c8fe90e
msesno <msesno@gmail.com>
2019-05-31 00:09:58 -0400

    put working files

---
+++ client/build/precache-manifest.0a43d964a04e0b22997bba8490b0483e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8520eabc26aee8957774",
+    "url": "/static/css/main.9e7887ad.chunk.css"
+  },
+  {
+    "revision": "8520eabc26aee8957774",
+    "url": "/static/js/main.acd250de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c627c64b2f590a238f23",
+    "url": "/static/js/2.13323e8c.chunk.js"
+  },
+  {
+    "revision": "8fe3ce5846829544acd32003ed1bdfef",
+    "url": "/index.html"
+  }
+];
